98403b5ab2487ba4f3da77c5373a10faf3ed9851
Jakob Jul Elben <elbenjakobjul@gmail.com>
2018-06-24 19:15:11 +0200

    Look into all subfolders

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     include_package_data=True,
     keywords='pydst',
     name='pydst',
-    packages=find_packages(include=['pydst']),
+    packages=find_packages(),
     setup_requires=setup_requirements,
     test_suite='tests',
     tests_require=test_requirements,
