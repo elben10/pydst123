60892265d5d2967e8890bc34750f4febd086f5ad
Jakob Jul Elben <elbenjakobjul@gmail.com>
2018-07-28 15:49:39 +0200

    Global definition of authors, email and implement git versioning

--- pydst/__init__.py
+++ pydst/__init__.py
@@ -1,11 +1,17 @@
 # -*- coding: utf-8 -*-
+"""Top-level package for Pydst."""
 
 from . import Connection
 from . import Cli
+from subprocess import Popen, PIPE
 
 
-"""Top-level package for Pydst."""
+def _git_version():
+    process = Popen(["git", "describe"], stdout=PIPE)
+    out, _ = process.communicate()
+    return ".".join(out.decode("utf-8").split("-", 1)[0][1:].split(".")[:3])
+
 
 __author__ = """Jakob Jul Elben, Kristian Urup Larsen"""
-__email__ = 'elbenjakobjul@gmail.com, kristianuruplarsen@gmail.com'
-__version__ = '0.1.0.9000'
+__email__ = "elbenjakobjul@gmail.com, kristianuruplarsen@gmail.com"
+__version__ = _git_version()
